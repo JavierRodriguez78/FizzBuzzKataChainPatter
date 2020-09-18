5dab8ab81ff6f0e35cacc7aca178fc20a2687516
Xavi <xavi@geekshubs.com>
2020-09-18 08:43:36 +0200

    [GREEN]Implemented isBuzz Method

--- src/fizz-buzz.ts
+++ src/fizz-buzz.ts
@@ -4,6 +4,11 @@ class FizzBuzz{
        if (number===3) return true;
        return false;
     }
+
+    static isBuzz(number:number): boolean {
+        if (number%5===0) return true;
+        return false;
+     }
 }
 
 export default FizzBuzz
