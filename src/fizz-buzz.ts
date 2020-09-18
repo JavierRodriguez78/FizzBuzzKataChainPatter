55168c862081efd32ae0406dc9582c0d4de98346
Xavi <xavi@geekshubs.com>
2020-09-18 08:56:55 +0200

    [BLUE] Refactor magic numbers

--- src/fizz-buzz.ts
+++ src/fizz-buzz.ts
@@ -1,12 +1,17 @@
 class FizzBuzz{
 
+     
+    static FizzFactor: number=3;
+    static BuzzFactor: number=5;
+
+
     static isFizz(number:number): boolean {
-       if (number%3===0) return true;
+       if (number% this.FizzFactor===0) return true;
        return false;
     }
 
     static isBuzz(number:number): boolean {
-        if (number%5===0) return true;
+        if (number%this.BuzzFactor===0) return true;
         return false;
      }
 
