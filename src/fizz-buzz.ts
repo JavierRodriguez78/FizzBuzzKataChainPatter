bac7dfe9528b2d34d94cda2560a09576e5e902ac
Xavi <xavi@geekshubs.com>
2020-09-18 08:40:31 +0200

    [GREEN]Implemented IsFizz method

---
+++ src/fizz-buzz.ts
@@ -0,0 +1,9 @@
+class FizzBuzz{
+
+    static isFizz(number:number): boolean {
+       if (number===3) return true;
+       return false;
+    }
+}
+
+export default FizzBuzz
--- test/fizz-buzz.test.ts
+++ test/fizz-buzz.test.ts
@@ -3,7 +3,7 @@ import FizzBuzz from "../src/fizz-buzz"
 describe('Fill Buzz', () => {
 
   it('It Should Return Fizz if Divisible By Three ', () => {
-    expect(FizzBuzz.issFizz(3)).toEqual(true)
+    expect(FizzBuzz.isFizz(3)).toEqual(true)
   })
 
 })
