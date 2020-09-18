ef4bf6d905353862b2246c22fd1238c2c7703c37
Xavi <xavi@geekshubs.com>
2020-09-18 08:42:04 +0200

    [RED] Implemented Buz Test

--- test/fizz-buzz.test.ts
+++ test/fizz-buzz.test.ts
@@ -5,5 +5,8 @@ describe('Fill Buzz', () => {
   it('It Should Return Fizz if Divisible By Three ', () => {
     expect(FizzBuzz.isFizz(3)).toEqual(true)
   })
+  it('It Should Return Buzz if Divisible By Five ', () => {
+    expect(FizzBuzz.isBuzz(5)).toEqual(true)
+  })
 
 })
